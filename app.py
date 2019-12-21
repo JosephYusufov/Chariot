f5a13d3e8b5fb267cf6c16cfeb9a760a128114a0
Moody Rahman <mrahman01@stuy.edu>
2019-12-21 17:53:13 -0500

    login skeleton

--- app.py
+++ app.py
@@ -11,3 +11,25 @@ import random
 import csv
 import sqlite3
 import os
+
+app = Flask(__name__)  # create instance of class Flask
+app.secret_key = "dfsgdfg"
+
+
+
+@app.route("/")  # assign following fxn to run when root route requested
+def index():
+    return render_template('index.html')
+
+
+@app.route("/login")
+def login():
+    if "username" in session:
+        return redirect("/welcome")
+    else:
+        return render_template("login.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    cache.cache()
+    app.run()
