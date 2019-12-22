1abb127ff8b99d3bae9ec569f774e5af3b2f382e
Moody Rahman <mrahman01@stuy.edu>
2019-12-21 20:40:38 -0500

    sql handler

--- app.py
+++ app.py
@@ -12,9 +12,34 @@ import csv
 import sqlite3
 import os
 
+
+
+
+class SQLHandler(object):
+    """docstring for SQLHandler."""
+
+    def __init__(self, dbname):
+        super(SQLHandler, self).__init__()
+        self.DB_FILE = dbname
+        self.db = sqlite3.connect(self.DB_FILE)  # open if file exists, otherwise create
+        self.c = self.db.cursor()  # facilitate db ops
+
+    def run(self, command):
+        self.c.execute(command)
+        if "select" in command.lower():
+            return self.c.fetchall()
+        self.db.commit()  # save changes
+
+    def dbclose(self):
+        self.db.close()  # close database
+
+
+
+
+
 app = Flask(__name__)  # create instance of class Flask
 app.secret_key = "dfsgdfg"
-
+db = SQLHandler("data.db")
 
 
 @app.route("/")  # assign following fxn to run when root route requested
@@ -29,7 +54,13 @@ def login():
     else:
         return render_template("login.html")
 
+
+# @app.route("/auth")
+
+
 if __name__ == "__main__":
-    app.debug = True
-    cache.cache()
-    app.run()
+    db.run("insert into loginfo VALUES('kek', 'doom')")
+    print(db.run("select * from loginfo"))
+    # app.debug = True
+    # cache.cache()
+    # app.run()
