f1a0a1c2ffeebc3cbebb14438d18e026c4e1178d
William Cao <wcao00@stuy.edu>
2020-01-13 23:37:15 -0500

    fixed routing

--- static/welcome_script.js
+++ static/welcome_script.js
@@ -2,10 +2,10 @@ $.get("/itinerary/list", (response) => {
     for(const itinerary of response.itineraries){
         const name = itinerary.name;
         const id = itinerary.id;
-        const date = itineary.date;
+        const date = itinerary.date;
 
         $("#itinerary-container").append($(`
-          <a href="/itinerary/${id}" class="list-group-item list-group-item-action">${name} -- ${date}</a>
+          <a href="/itinerary/view" class="list-group-item list-group-item-action">${name} -- ${date}</a>
         `))
     }
 });
