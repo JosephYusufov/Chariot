5a1590e5d83f2b16ab88fff7bab3f9319c7869ce
Moody Rahman <mrahman01@stuy.edu>
2020-01-14 21:54:25 -0500

    paren

--- app.py
+++ app.py
@@ -155,7 +155,7 @@ def itinerary_view(id):
                 if x == "Food":
                     out_dict["events"].append({"name":"mamma mia meatballs", "address":"One Infinite Loop, CA", "time_start":"2020-01-15T02:29:56.647Z", "time_end":"2020-01-15T02:29:56.647Z"})
                 if x == Park:
-                    out_dict["events"].append({"name":"heroin park", "address":"69 420ST", "time_start":"2020-01-15T02:29:56.647Z", "time_end":"2020-01-15T02:29:56.647Z"}))
+                    out_dict["events"].append({"name":"heroin park", "address":"69 420ST", "time_start":"2020-01-15T02:29:56.647Z", "time_end":"2020-01-15T02:29:56.647Z"})
                 else:
                     out_dict["events"].append({"name":"hell", "address":"davey jones locker", "time_start":"2020-01-15T02:29:56.647Z", "time_end":"2020-01-15T02:29:56.647Z"})
             return json.dumps(out_dict)
